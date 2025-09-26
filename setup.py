18dbde79f980dee6931e65295bd95a9526ffdeea
Chris Lalancette <clalancette@gmail.com>
2025-09-26 09:10:07 -0400

    Update setup.py
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     license='BSD',
     extras_require={
         'test': ['pytest'],
-    }, 
+    },
     entry_points={
         'console_scripts': [
             'teleop_twist_keyboard = teleop_twist_keyboard:main'
